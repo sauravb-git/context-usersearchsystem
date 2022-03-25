32ff165431ca979658d7cd584563369f3a409e68
saurav-git <pronaybarua9@gmail.com>
2022-03-25 12:42:52 +0600

    U

---
+++ UserSearchSystem-master/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UserFind from './components/UserFind';
+
+function App() {
+  return (
+    <div className="App">
+      <UserFind />
+    </div>
+  );
+}
+
+export default App;
